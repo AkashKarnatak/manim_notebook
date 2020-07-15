cd739f8244884142963b64a1949bb172c1757610
AkashKarnatak <akashkarnatak.ak47@gmail.com>
2020-07-15 20:03:43 +0530

    Updated setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="manim_notebook",
-    version="1.0.0",
+    version="1.0.1",
     description="Integrates manim with jupyter notebook",
     long_description=long_description,
     long_description_content_type="text/markdown",
