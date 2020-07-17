4b7ab30832d3bb02efbcb3acf44f06e954821a52
AkashKarnatak <akashkarnatak.ak47@gmail.com>
2020-07-17 13:15:58 +0530

    Modified install_manim_dependencies_on_colab.sh

--- install_manim_dependencies_on_colab.sh
+++ install_manim_dependencies_on_colab.sh
@@ -1,5 +1,5 @@
-!apt-get update
-!apt-get upgrade
-!apt install ffmpeg sox
-!apt-get install libcairo2-dev libjpeg-dev libgif-dev
-!apt install texlive-latex-base texlive-full texlive-fonts-extra
+apt-get update
+apt-get upgrade
+apt install ffmpeg sox
+apt-get install libcairo2-dev libjpeg-dev libgif-dev
+apt install texlive-latex-base texlive-full texlive-fonts-extra
